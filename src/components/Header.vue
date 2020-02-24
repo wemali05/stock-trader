e33a574562666cd70bfe4f1717fbc912e377d932
wemali05 <alfredwemali05@gmail.com>
2020-02-24 16:13:34 +0300

    adding navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Home template</h1>
+   <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <router-link to="/" class="navbar-brand">Stock trader</router-link>
+    </div>
+
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <router-link to="/portfolio" activeclass="active" tag="li"><a>Portfolio</a></router-link>
+        <router-link to="/stocks" activeclass="active" tag="li"><a>Stocks</a></router-link>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">End Day</a></li>
+        <li class="dropdown">
+          <a href="#" 
+                class="dropdown-toggle" 
+                data-toggle="dropdown" 
+                role="button" aria-haspopup="true" 
+                aria-expanded="false">Save and Load <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Save Data</a></li>
+            <li><a href="#">Load Data</a></li>
+           </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 </template>
