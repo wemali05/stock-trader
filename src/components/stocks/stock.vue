3c3bbcc99871f71406034e7f59c4727080bacbed
wemali05 <alfredwemali05@gmail.com>
2020-02-24 19:48:39 +0300

    stocks component

--- src/components/stocks/stock.vue
+++ src/components/stocks/stock.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Stocks -stock template</h1>
+   <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                Name
+                <small>(price: PRICE)</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input
+                          type="number"
+                          class="form-control"
+                          placeholder="Quantity">
+                </div>
+                <div class="pull-right">
+                    <button class="btn btn-success">Buy</button>
+                </div>
+            </div>
+        </div>
+   </div>
 </template>
+<script>
+export default {
+
+}
+</script>
--- src/components/stocks/stocks.vue
+++ src/components/stocks/stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>Stocks -template</h1>
+    <div>
+         <app-stock v-for="stock in stocks"></app-stock>
+    </div>
 </template>
+
+<script>
+import Stock from './Stock.vue';
+
+export default {
+    data(){
+      return {
+            stocks: [
+            {id: 1, name: 'BMW', price: '110'},
+            {id: 2, name: 'Google', price: '200'},
+            {id: 3, name: 'Apple', price: '2500'},
+            {id: 4, name: 'twitter', price: '8'},
+        ]
+      }
+    },
+    components: {
+        appStock: Stock
+    }
+}
+</script>
