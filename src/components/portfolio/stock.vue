2b114c885703a29353f9feadfa9690bc18da3ca5
wemali05 <alfredwemali05@gmail.com>
2020-02-25 14:22:23 +0300

    Adding some checks

--- src/components/portfolio/stock.vue
+++ src/components/portfolio/stock.vue
@@ -13,12 +13,14 @@
                           type="number"
                           class="form-control"
                           placeholder="Quantity"
-                          v-model="quantity">
+                          v-model="quantity"
+                          :class="{danger: insufficientQuantity}">
                 </div>
                 <div class="pull-right">
                     <button class="btn btn-info"
                             @click="sellStock"
-                            :disabled="quantity <= 0 || ! !Number.isInteger(quantity)">Sell</button>
+                            :disabled="insufficientQuantity || quantity <= 0 || ! !Number.isInteger(quantity)"
+                            >{{ insufficientQuantity ? 'Not Enough' : 'Sell'}}</button>
                 </div>
             </div>
         </div>
@@ -32,6 +34,11 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed: {
+        insufficientQuantity(){
+            return this.quantity > this.stock.quantity
         }
     },
     methods: {
@@ -50,3 +57,9 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.danger{
+    border: 1px solid red;
+}
+</style>
--- src/components/stocks/stock.vue
+++ src/components/stocks/stock.vue
@@ -13,12 +13,14 @@
                           type="number"
                           class="form-control"
                           placeholder="Quantity"
-                          v-model="quantity">
+                          v-model="quantity"
+                          :class="{danger: insufficientFunds}">
                 </div>
                 <div class="pull-right">
                     <button class="btn btn-success"
                             @click="buyStock"
-                            :disabled="quantity <= 0 || ! !Number.isInteger(quantity)">Buy</button>
+                            :disabled="insufficientFunds || quantity <= 0 || ! !Number.isInteger(quantity)"
+                            >{{ insufficientFunds ? 'insufficient Funds' : 'Buy'}}</button>
                 </div>
             </div>
         </div>
@@ -30,6 +32,14 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed: {
+        funds(){
+            return this.$store.getters.funds
+        },
+        insufficientFunds(){
+            return this.quantity * this.stock.price > this.funds;
         }
     },
     methods: {
@@ -46,3 +56,9 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.danger{
+    border: 1px solid red;
+}
+</style>
